5734cba262aad5761f52bd7280e151d5b7e279cd
steph <stephaniehu0207@gmail.com>
2021-01-10 17:59:00 -0800

    sticky navi and scrolling for hero buttons added

---
+++ resources/js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    /* For the sticky navigation */
+    $('.section-features').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+
+    /* Scroll on buttons */
+    $('.scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.section-plans').offset().top}, 1000);
+    });
+
+    $('.scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.section-features').offset().top}, 1000);
+    });
+
+    /* Navigation scroll */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
